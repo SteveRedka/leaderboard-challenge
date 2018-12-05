24b499ef892900293f9a0e766bfda2ca896d4679
Steve Redka <cogitofallacy@gmail.com>
2018-12-05 14:45:21 +0300

    step 3: wip

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,5 +1,16 @@
 class ShowsController < ApplicationController
+  protect_from_forgery except: :refresh_results
   def index
     @shows = Shows::OrderedByAverageReviewsScoreQuery.new.call
+  end
+
+  def refresh_results
+    execution_end = Time.now + 1.5
+    @shows = Shows::OrderedByAverageReviewsScoreQuery.new.call
+    sleep execution_end - Time.now
+
+    respond_to do |format|
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'shows#index'
+  namespace :shows do
+    get :refresh_results
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
